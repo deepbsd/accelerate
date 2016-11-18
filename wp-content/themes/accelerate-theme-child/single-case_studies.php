d9438d54e9150045ead1e801453faae85ca57f11
David S. Jackson <deepbsd@yahoo.com>
2016-11-18 12:51:59 -0500

    updates for the week

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -32,18 +32,15 @@ get_header(); ?>
 		</aside>
 
     <div class="case-study-images">
-			<?php if($image_1) { ?>
-				<!-- <img src="<?php echo $image_1; ?>" /> -->
-				<?php echo wp_get_attachment_image( $image_1, $size ); ?>
-      <?php } ?>
-			<?php if($image_2) { ?>
-				<!-- <img src="<?php echo $image_2; ?>" /> -->
-				<?php echo wp_get_attachment_image( $image_2, $size ); ?>
-			<?php } ?>
-			<?php if($image_3) { ?>
-				<!-- <img src="<?php echo $image_3; ?>" /> -->
-				<?php echo wp_get_attachment_image( $image_3, $size ); ?>
-			<?php } ?>
+			<?php if($image_1) {
+				echo wp_get_attachment_image( $image_1, $size );
+       } ?>
+			<?php if($image_2) {
+				echo wp_get_attachment_image( $image_2, $size );
+			 } ?>
+			<?php if($image_3) {
+				echo wp_get_attachment_image( $image_3, $size );
+			 } ?>
 		</div>
 
 	</article>
